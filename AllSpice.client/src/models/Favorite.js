e7c99d8bcb3bd28da87cb40000b6247bbf8bab78
Chandler Bowman <doctorgrant99@gmail.com>
2023-04-30 17:32:39 -0600

    client models

---
+++ AllSpice.client/src/models/Favorite.js
@@ -0,0 +1,8 @@
+export class Favorite {
+    constructor(data) {
+        this.id = data.id
+        this.accountId = data.accountId
+        this.recipeId = data.recipeId
+        this.favoriteId = data.favoriteId
+    }
+}
